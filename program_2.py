67a70ebc3f3dcbeeb1e82af83268424ae898f383
notGriffin <krima724@gmail.com>
2025-09-05 16:01:21 -0500

    program 2.py

--- program_2.py
+++ program_2.py
@@ -1,6 +1,14 @@
 
 def average_age():
-    # Get User Input
+    age1 = int(input("Friend 1 age: "))
+    age2 = int(input("Friend 2 age: "))
+    age3 = int(input("Friend 3 age: "))
+    age4 = int(input("Friend 4 age: "))
+    age5 = int(input("Friend 5 age: "))
+
+    avrageAge = (age1 + age2 + age3 + age4 + age5) / 5
+
+    print (avrageAge)
 
 
     # Sum ages
